a68fa8433b018cf63958b68b1797a08b43ddb162
KelvinHarmenSyahputra <kelvinsyaputra01@gmail.com>
2023-10-07 21:53:24 +0700

    update malam minggu

--- static/js/main.js
+++ static/js/main.js
@@ -160,10 +160,7 @@ function listing() {
   <td>${category}</td> 
   <td>${description}</td> 
   <td>${bestseller}</td> 
-  <td><a
-        href="/adminpanel/posting/${num}" class="btn btn-success mb-2">
-        <i class="fa fa-search fa-sm"></i>
-    </a>
+  <td>
     <button type="button" class="btn btn-warning mb-2"
         data-bs-toggle="modal" data-bs-target="#editdataDetail"
         onclick="updatePost('${num}')">
@@ -227,3 +224,72 @@ function menu() {
     },
   });
 }
+
+
+// posting faq
+function posting_faq() {
+  let title = $("#input-title").val().trim();
+  let file = $("#input-file").prop("files")[0];
+  let description = $("#input-description").val();
+  let category = $("#layout-category").val();
+  let bestseller = $("#layout-bestseller").val();
+  let price = $("#input-price").val();
+  
+  
+  if (!title || !file || !description || !category || !bestseller || !price ) {
+    alert("Mohon lengkapi data dengan benar");
+    return;
+  }
+
+  // Validasi tipe file (hanya menerima gambar)
+  if (!file.type.startsWith("image/") || file.type === "image/gif") {
+    alert("Mohon pilih file gambar!");
+    return;
+  }
+
+  // Validasi kapasitas file (maksimum 2 megabyte)
+  if (file.size > 2 * 1024 * 1024) {
+    alert("Ukuran file terlalu besar, maksimum 2 megabyte diperbolehkan");
+    return;
+  }
+  
+  // Membuat objek gambar untuk memeriksa ukuran
+  let image = new Image();
+  image.src = URL.createObjectURL(file);
+  
+  image.onload = function () {
+    // Validasi ukuran width dan height
+    if (image.width >= 593 && image.height >= 420) {
+
+
+      let form_data = new FormData();
+      form_data.append("title_give", title);
+      form_data.append("file_give", file);
+      form_data.append("description_give", description);
+      form_data.append("category_give", category);
+      form_data.append("bestseller_give", bestseller);
+      form_data.append("price_give", price); // Menambahkan nilai layout ke formData
+      
+      $.ajax({
+        type: "POST",
+        url: "/adminmenu/postingmenu",
+        data: form_data,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+          alert(response["msg"]);
+          window.location.reload();
+        },
+      });
+    } else {
+      alert("Ukuran gambar harus minimal 593px lebar dan 420px tinggi");
+    }
+  };
+
+  image.onerror = function () {
+    alert(
+      "Gagal memuat gambar. Pastikan file yang dipilih adalah gambar yang valid."
+      );
+    };
+    
+  }
